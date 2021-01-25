0339641a3e0682f6686239f03dad03e23eaeeb36
Muhammad Charaf <muhammad.charaf@gmail.com>
2021-01-25 21:22:04 +0200

    adding secrets

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,23 +1,20 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-
-import { /*verify ,*/ decode } from 'jsonwebtoken'
+import { APIGatewayTokenAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import * as AWS from 'aws-sdk';
+import { verify , decode, JsonWebTokenError } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-// import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
-
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-// const jwksUrl = '...'
+const auth0SecretId = process.env.AUTH0_SECRET_ID;
+const auth0SecretField = process.env.AUTH0_SECRET_FIELD;
+const secretsManager = new AWS.SecretsManager();
+let cachedSecret: string;
 
 export const handler = async (
-  event: CustomAuthorizerEvent
+  event: APIGatewayTokenAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
@@ -55,13 +52,17 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  if (!authHeader) {
+    throw new Error('Invalid Auth Header');
+  }
+  if (!authHeader.toLocaleLowerCase().startsWith('bearer ')) {
+    throw new Error('Invalid AuthHeader Format');
+  }
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const token = getToken(authHeader)
+  const clientSecret = (await getSecret())[auth0SecretField];
+
+  return verify(token, clientSecret) as JwtPayload;
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +75,14 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+async function getSecret() {
+  if (cachedSecret) 
+    return cachedSecret;
+
+  const data = await secretsManager.getSecretValue( { SecretId: auth0SecretId }).promise();
+  cachedSecret = data.SecretString;
+
+  return JSON.parse(cachedSecret);
 }
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,8 +1,11 @@
 import 'source-map-support/register'
-
+import * as AWS from 'aws-sdk';
+import Response from '../../models/response'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-  return null;
+  const todoTableName = process.env.TODO_TABLE;
+  const dbContext = new AWS.DynamoDB.DocumentClient();
+
+  return undefined;
 }
