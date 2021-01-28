49974fe628d51009a9d7957f169ea270f0545207
Muhammad Charaf <muhammad.charaf@gmail.com>
2021-01-29 01:24:11 +0200

    Delete

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,24 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getUserIdFromToken} from '../../auth/utils'
+import * as AWS from 'aws-sdk';
+import response from '../../models/response';
+const tableName = process.env.TODO_TABLE;
+const dbClient = new AWS.DynamoDB.DocumentClient();
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // const todoId = event.pathParameters.todoId
+  const todoId = event.pathParameters.todoId
+  const token = event.headers.Authorization;
+  const userId = getUserIdFromToken(token);
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  await dbClient.delete({
+    TableName: tableName,
+    Key: {
+      'todoId': todoId,
+      'userId': userId
+    }
+  }).promise();
+
+  return response.success();
 }
